4316ca4fee9c7c6b71d3e0a7db2e29473e895693
Rohit Kharche <111701144+rohitrokz29@users.noreply.github.com>
2023-12-12 12:41:42 +0530

    added tab navigation

--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -4,10 +4,13 @@ import { NavigationContainer } from "@react-navigation/native";
 import Home from "../components/Home";
 import CreateUser from "../components/CreateUser";
 import UserProfile from "../components/UserProfile";
-import Subject from "../components/Subject";
 import { useUserContext } from "../context/UserData";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Feather } from "@expo/vector-icons";
+import SubjectList from "../components/SubjectList";
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 const GetStartedScreens = () => {
     return (
@@ -20,10 +23,44 @@ const GetStartedScreens = () => {
 
 const AppScreens = () => {
     return (
-        <Stack.Navigator>
-            <Stack.Screen name='userProfile' component={UserProfile} />
-            <Stack.Screen name='subject' component={Subject} />
-        </Stack.Navigator>
+        <Tab.Navigator
+            initialRouteName="Subjects"
+            tabBarOptions={{
+                activeTintColor: '#ff8070',
+                inactiveTintColor: "#fff",
+                labelStyle: {
+                    fontSize: 15,
+                },
+                tabStyle: {
+                    backgroundColor: "#414141"
+                }
+            }}
+            screenOptions={{
+                headerShown:false
+            }}
+>
+            <Tab.Screen
+                name='Subjects'
+                component={SubjectList}
+                options={{
+                    tabBarLabel: "Subjects",
+                    tabBarIcon: ({ color, size }) => (
+                        <Feather name="list" size={size} color={color} />)
+                }}
+            />
+            <Tab.Screen
+                name='Profile'
+                component={UserProfile}
+                options={{
+                    tabBarLabel: "Profile",
+                    tabBarIcon: ({ color, size }) => (
+                        <Feather name="user" size={size} color={color} />
+                    ),
+
+                }}
+            />
+        
+        </Tab.Navigator>
     )
 }
 
